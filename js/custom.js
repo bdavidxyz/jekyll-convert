14d0fc7f578f18ac4d674142e5ffa39f17e9b3c2
David Borow <davbohr@gmail.com>
2016-02-07 23:17:55 +0100

    integrating h1

--- js/custom.js
+++ js/custom.js
@@ -13,4 +13,38 @@ $(function() {
         $('.menu-content').css('visibility','hidden');
       }
     });
+
+
+
+    var theater = theaterJS({maxSpeed:250});
+
+    theater
+    .on('type:start, erase:start', function () {
+      var currentActor = theater.getCurrentActor();
+      var currentActorElement = currentActor.$element;
+
+      currentActorElement.classList.add('blinking-caret');
+    })
+    .on('type:end, erase:end', function () {
+      var currentActor = theater.getCurrentActor();
+      var currentActorElement = currentActor.$element;
+
+      currentActorElement.classList.remove('blinking-caret');
+    });
+
+    theater
+    .addActor('js-animate-hero', {accuracy:1})
+    .addScene('js-animate-hero:create web applications')
+    .addScene(1600)
+    .addScene('js-animate-hero:automate growth hacks')
+    .addScene(1600)
+    .addScene('js-animate-hero:design user interfaces')
+    .addScene(1600)
+    .addScene('js-animate-hero:build corporate website')
+    .addScene(1600)
+    .addScene(theater.replay);
+
+
   });
+
+
