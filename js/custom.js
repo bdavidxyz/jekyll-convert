50a5e9654ade2d7c13f5c99a503d7892effa0be6
David Borow <davbohr@gmail.com>
2016-02-07 22:13:23 +0100

    correctly handled SVG images

--- js/custom.js
+++ js/custom.js
@@ -4,35 +4,13 @@ $(function() {
 
     // Toggle
     hamburgertoggle.click(function(e) {
-      var top  = window.pageYOffset || document.documentElement.scrollTop;
-      console.log('top : ' + top);
       e.preventDefault();
       $(this).toggleClass('is-active');
-      // $('.menu-content').css({ 'top' : top + 'px' });
-      // $('.menu-content').css({ 'display' : '' });
       if ( $('.menu-content').css('visibility') === 'hidden') {
         $('.menu-content').css('visibility','visible');
       }
       else{
         $('.menu-content').css('visibility','hidden');
       }
-
-      // $('.menu-content').toggle();
-
-    // $('.menu-content').css({ 'visibility' : 'visible' });
-
-    // $('#element').on('scroll touchmove mousewheel', function(e){
-    //   e.preventDefault();
-    //   e.stopPropagation();
-    //   return false;
-    // });
-
-      // $('.overlay').toggleClass('is-active');
-     //  if ($('.overlay.is-active').length > 0) {
-     //    $('.overlay.is-active').css({ 'top' : top + 'px' });
-     //  } else {
-     //   $('.overlay').css({'top':''});
-     // }
-        // $('.overlay') = '';
-      });
+    });
   });
